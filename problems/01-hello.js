e8ecba358e7392cc65e1d9c6d7b094a8d30f4b11
rmmillay <rmmillay@gmail.com>
2024-06-08 14:12:34 -0700

    gitignore added

--- problems/01-hello.js
+++ problems/01-hello.js
@@ -5,13 +5,16 @@ HINT: this function doesn't need to return anything, it should just print using
 console.log.
 */
 
-// Your code here 
+// Your code here
 
 // Note: If you console.log the return value of these function calls; you will get undefined. Remember a function that does not return anything evaluates to undefined.
 
 // hello("cat"); // "Hello, cat"
 // hello("Helen"); // "Hello, Helen"
 // hello("Sting"); // "Hello, Sting"
+
+git remote set-url origin https://github.com/rmmillay/aa01-function-parameters
+git push -u origin
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = hello;
